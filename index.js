4ed411fd00e2dbd596e4bdaff037f3031d9ff503
Samuel Castillo <73082164+samuelcastillogt@users.noreply.github.com>
2023-10-19 19:07:35 -0600

    inicio

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const data = require("./data/index.json")
+const app = express()
+app.use(express.json())
+app.get("/", (req, res)=>{
+    res.send(data)
+})
+app.get("/type/:type", async(req, res)=>{
+    const {type} = req.params
+    const dataNude = await data.data.filter((item)=> item.frameType == type)
+    res.send(dataNude.slice(0, 10))
+})
+app.get("/card/:id", (req, res)=>{
+    const {id} = req.params
+    const dataFilter = data.data.filter((item)=> item.id == id)
+    console.log(dataFilter)
+    res.send(dataFilter.slice(0,10))
+})
+app.listen(5000, ()=> console.log("activefrom the hell"))
